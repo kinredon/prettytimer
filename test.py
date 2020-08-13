f53c25b41213b7ab0360ad0e0b5cabb29fedad01
zhengjiawen <zhengjiawen77@gmail.com>
2020-08-13 16:55:50 +0800

    set mode in collect

--- test.py
+++ test.py
@@ -4,23 +4,23 @@ from prettytimer import PrettyTimer
 timer = PrettyTimer()
 
 timer.start('load_data')
-time.sleep(3)
+time.sleep(0.3)
 timer.end('load_data')
 
 timer.start('load_data')
-time.sleep(1)
+time.sleep(0.1)
 timer.end('load_data')
 
 timer.start('forward')
-time.sleep(3)
+time.sleep(0.3)
 timer.end('forward')
 
 timer.start('backward')
-time.sleep(3)
+time.sleep(0.3)
 timer.end('backward')
 
 timer.start('clloct')
-time.sleep(3)
+time.sleep(0.3)
 timer.end('clloct')
 
-timer.collect()
+timer.collect(mode='sum')
